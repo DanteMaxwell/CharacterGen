245e2482f7838dc6ce91af2f886eefd2c706cdea
DanteMaxwell <LaughingMan32286@gmail.com>
2014-05-20 13:27:27 -0400

    Planned Mechanic - Reaction
    To learn about the planned functionality of the Reaction mechanic, open
    the .txt file.

---
+++ lib/chargen/reaction.rb
@@ -0,0 +1,3 @@
+#NOTE:  This is initially a placeholder on how to calculate initial reactions for NPC type characters, code implementation will occur at a later point
+
+#See text file reaction_mechanic.txt for details on the mechanic's planned functionality.
